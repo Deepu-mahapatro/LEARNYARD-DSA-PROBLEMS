3733711aecb598bc4a67fa26d1b395057b11f705
deepu <deepumahapatro2006@gmail.com>
2026-06-25 20:48:56 +0530

    REMOVE DUPLICATES FROM SORTED ARRAY

---
+++ TWO_POINTERS/PROBLEM26.py
@@ -0,0 +1,13 @@
+#REMOVE DUPLICATES FROM SORTED ARRAY
+
+def duplicate(nums):
+    if len(nums)==0:
+        return -1
+    i=0
+    for j in range(1,len(nums)):
+        if nums[i]!=nums[j]:
+            i+=1
+            nums[i]=nums[j]
+    return i+1
+nums=[2,2,3,4]
+print(duplicate(nums))
