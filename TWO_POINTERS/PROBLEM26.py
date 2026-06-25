e9363e9c97b7bd387d5cc88f6ba97f0cc7ac367d
deepu <deepumahapatro2006@gmail.com>
2026-06-25 21:25:57 +0530

    REMOVE DUPLICATES FROM SORTED ARRAY

--- TWO_POINTERS/PROBLEM26.py
+++ TWO_POINTERS/PROBLEM26.py
@@ -1,13 +1,24 @@
 #REMOVE DUPLICATES FROM SORTED ARRAY
 
-def duplicate(nums):
-    if len(nums)==0:
-        return -1
-    i=0
-    for j in range(1,len(nums)):
-        if nums[i]!=nums[j]:
-            i+=1
-            nums[i]=nums[j]
-    return i+1
+from typing import List
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        #EDGE CASE 
+        #IF ARRAY IS EMPTY
+        if len(nums)==0:
+            return 0
+        #SLOW POINTER (POINTS TO UNIQUE ELEMENT)
+        i=0
+        #FAST POINTER
+        #TRAVERSE THE ARRAY TO FIND UNIQUE ELEMENTS
+        for j in range(1,len(nums)):
+            #IF NEW UNIQUE ELEMENT
+            if nums[i]!=nums[j]:
+                #INCREASE THE POINTER
+                i+=1
+                #PLACE UNIQUE ELEMENT AT THE SLOW POINTER POSITION
+                nums[i]=nums[j]
+        return i+1
+obj=Solution()
 nums=[2,2,3,4]
-print(duplicate(nums))
+print(obj.removeDuplicates(nums))
